7ea9d3cca6dd0875680afdc45ce2394dbc580de0
Israel <g9931526@gmail.com>
2025-03-19 11:07:00 +0200

    kiosk desktop

---
+++ kioskIkon/src/main.js
@@ -0,0 +1,14 @@
+const { Command } = window.__TAURI__.shell;
+
+async function run_script() {
+  let result = await Command.create('exec-cscript').execute();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const icon = document.getElementById('icon');
+  
+  // Add the event listener to the icon
+  icon.addEventListener('click', run_script);
+
+  run_script();
+});
