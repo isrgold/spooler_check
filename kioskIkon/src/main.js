ab5cbe12b863a8005aa24b929c54386bad752b1e
Israel <g9931526@gmail.com>
2025-03-19 13:52:34 +0200

    Update main.js

--- kioskIkon/src/main.js
+++ kioskIkon/src/main.js
@@ -6,9 +6,15 @@ async function run_script() {
 
 window.addEventListener("DOMContentLoaded", () => {
   const icon = document.getElementById('icon');
-  
+
   // Add the event listener to the icon
   icon.addEventListener('click', run_script);
+
+  document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault()
+    })
+  })
 
   run_script();
 });
